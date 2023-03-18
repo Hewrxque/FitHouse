68b52386cd4e853c5faf83b3ac18b32b297f1b2d
Hewrxque <josehenriquevieiradacruz@gmail.com>
2023-03-18 12:06:27 -0300

    f

--- src/screens/Trainings/A/index.js
+++ src/screens/Trainings/A/index.js
@@ -4,6 +4,13 @@ import styles from './styles';
 export default function T_A({navigation}) {
   return (
     <SafeAreaView style={styles.container}>
+        <TouchableOpacity
+        style={styles.arrow}
+        onPress={() => {
+          navigation.goBack();
+        }}>
+        <Icon name={'arrow-back-outline'} size={40} color={'#ffffff'} />
+      </TouchableOpacity>
     <View style={{padding: 50}}>
      <Text>Treino A</Text>
     </View>
